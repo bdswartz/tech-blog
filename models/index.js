ff35e07241193d4aef5a28b43ed3dd6a1e8f86d0
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-04-03 17:08:32 -0400

    changed case on User and Post routes in index.js sequelize model file

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
-const User = require('./User');
-const Post = require('./Post');
+const User = require('./user');
+const Post = require('./post');
 const Comment = require('./comments');
 
 // create associations between users and posts
